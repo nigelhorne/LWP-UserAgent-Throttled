d5bb4a6d8341cefe1314fde2a14e6008a5d26439
Nigel Horne <njh@bandsman.co.uk>
2017-06-23 11:06:17 -0400

    Release 0.06.2

--- lib/LWP/UserAgent/Throttled.pm
+++ lib/LWP/UserAgent/Throttled.pm
@@ -12,11 +12,11 @@ LWP::UserAgent::Throttled - Throttle requests to a site
 
 =head1 VERSION
 
-Version 0.06.1
+Version 0.06.2
 
 =cut
 
-our $VERSION = '0.06.1';
+our $VERSION = '0.06.2';
 
 =head1 SYNOPSIS
 
