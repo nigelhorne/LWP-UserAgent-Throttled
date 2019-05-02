61805a2f97d28fd045b9025b32e22ae584f9d790
Nigel Horne <njh@bandsman.co.uk>
2019-05-02 09:27:19 -0400

    Added housekeeping tests

--- lib/LWP/UserAgent/Throttled.pm
+++ lib/LWP/UserAgent/Throttled.pm
@@ -124,10 +124,6 @@ You can also look for information at:
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=LWP-UserAgent-Throttled>
 
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/LWP-UserAgent-Throttled>
-
 =item * CPAN Ratings
 
 L<http://cpanratings.perl.org/d/LWP-UserAgent-Throttled>
